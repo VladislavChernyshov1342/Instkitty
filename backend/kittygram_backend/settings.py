982dcac812d7a3e243711e1ad4322d832103bbbc
Vladislav Chernyshov <vvv134231979WWW@yandex.ru>
2025-03-13 15:43:58 +0300

    add python dotenv two

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from django.core.management.utils import get_random_secret_key
+from django.core.management.utils import (
+	get_random_secret_key,
+)
 
 load_dotenv()
 
